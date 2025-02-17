3a41151423c145548e237faba8946e1db045fd0d
Sreejesh <sreejesh25122006@gmail.com>
2025-02-17 15:46:12 +0530

    Added Toggle Feature

--- FrontendDevelopment/CounterApp/src/App.jsx
+++ FrontendDevelopment/CounterApp/src/App.jsx
@@ -1,9 +1,23 @@
 import { useState, useRef, useEffect } from "react";
+import "./App.css"; // Import CSS
+import "@fortawesome/fontawesome-free/css/all.min.css"; // Import Font Awesome
 
 export default function App() {
   const [count, setCount] = useState(0);
   const prevCount = useRef(0);
   const nextprevCount = useRef(0);
+  const bodyRef = useRef(document.body); // Ref to the body element
+  const [isDark, setIsDark] = useState(false); // State for toggling icon
+
+  const toggleDarkMode = () => {
+    if (bodyRef.current.classList.contains("dark-mode")) {
+      bodyRef.current.classList.remove("dark-mode");
+      setIsDark(false);
+    } else {
+      bodyRef.current.classList.add("dark-mode");
+      setIsDark(true);
+    }
+  };
 
   useEffect(() => {
     // Update refs whenever count changes
@@ -33,6 +47,13 @@ export default function App() {
       <button onClick={handleIncrement}>Increment</button>
       <button onClick={handleDecrement}>Decrement</button>
       <button onClick={handleReset}>Reset</button>
+
+       
+      <i 
+        className={`toggle fa-solid ${isDark ? "fa-toggle-on" : "fa-toggle-off"}`} 
+        onClick={toggleDarkMode}>
+      </i>
+
     </div>
   );
 }
